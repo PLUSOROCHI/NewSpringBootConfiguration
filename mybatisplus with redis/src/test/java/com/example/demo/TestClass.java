738208b0796a046ca45348c597a222297385abe2
includeno <includeno@126.com>
2020-04-19 23:23:00 +0800

    mybatis with redis 注解实现

---
+++ mybatisplus with redis/src/test/java/com/example/demo/TestClass.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+
+//Spriong的测试环境
+@RunWith(SpringRunner.class)
+//classes填写项目启动类
+@SpringBootTest(classes=DemoApplication.class,webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestClass{
+    @Autowired
+    UserService userService;
+    @Test
+    public void test1(){
+        //从数据库查询所有用户
+        List<User> allUsers=userService.getAllUsers(null);
+        for(User user:allUsers){
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void test2(){
+        //添加用户
+        User a=new User();
+        a.setUsername("abcde");
+        a.setPassword("qojjdbvdmvkhynbjndc");
+        a.setCreateTime("sdbdfv");
+        a.setEmail("khjknojmjhg");
+        a.setNickname("jhddbfriewqepq");
+        userService.addUser(a);
+    }
+}
